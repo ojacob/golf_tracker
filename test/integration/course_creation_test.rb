1edee105ac2255c27a6806b339ce2b343f2a9d09
Olivier Jacob <ojacob@octo.com>
2010-04-21 13:56:31 +0200

    HoleInformation management (distance by tee shot color)

--- test/integration/course_creation_test.rb
+++ test/integration/course_creation_test.rb
@@ -31,10 +31,26 @@ class CourseCreationTest < ActionController::IntegrationTest
     18.times do |i|
       fill_in "hole_#{i+1}_par", :with => @pars[i]
       fill_in "hole_#{i+1}_handicap", :with => @handicaps[i]
+      @distances.each do |color, dists|
+        fill_in "hole_#{i+1}_info_#{color}", :with => dists[i]
+      end
     end
 
     click_button 'Create'
     assert_contain 'Course was successfully created.'
+    
+    # Assertions on course content
+    course = Course.find_by_name("L'aigle")
+    assert !course.nil?
+    assert_equal 72, course.par
+    assert_equal 18, course.holes.size
+    course.holes.each_with_index do |hole, i|
+      assert_equal @pars[i], hole.par
+      assert_equal @handicaps[i], hole.handicap
+      @distances.each do |color, dists|
+        assert_equal dists[i], hole.info(color).distance
+      end
+    end
   end
   
   test "update course card" do
@@ -44,12 +60,14 @@ class CourseCreationTest < ActionController::IntegrationTest
     fill_in "hole_2_handicap", :with => 16
     fill_in "hole_4_par", :with => 4
     fill_in "hole_4_handicap", :with => 2
+    fill_in "hole_6_info_white", :with => 180
     
     click_button "Save"
     
     assert_equal 3, @course.reload.holes[1].par
-    assert_equal 16, @course.reload.holes[1].handicap
+    assert_equal 16, @course.holes[1].handicap
     assert_equal 4, @course.holes[3].par
-    assert_equal 2, @course.reload.holes[3].handicap
+    assert_equal 2, @course.holes[3].handicap
+    assert_equal 180, @course.holes[5].info("white").distance
   end
 end
