f37c0e39c8f7379f600b57cd48013d46d781b691
Olivier Jacob <ojacob@octo.com>
2010-04-23 17:25:35 +0200

    Create more than one course in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+names = ["L'aigle", "L'albatros", "Paris International Golf Club"]
 pars = [4, 4, 4, 3, 4, 3, 5, 4, 4, 3, 4, 4, 3, 4, 5, 4, 5, 4]
 handicaps = [12, 2, 18, 16, 6, 14, 10, 8, 4, 9, 11, 1, 17, 5, 15, 3, 13, 7]
 distances = {
@@ -8,14 +9,16 @@ distances = {
   "red" => [282, 342, 205, 107, 316, 110, 395, 294, 284, 148, 326, 292, 116, 348, 360, 296, 373, 283]
 }
 
-aigle = Course.new(:name => "L'aigle", :par => 72)
-pars.each_with_index do |par, i|
-  aigle.holes.build(:par => par, :position => i+1, :handicap => handicaps[i])
-end
-distances.each do |color, dists|
-  dists.each_with_index do |d, i|
-    aigle.holes[i].hole_informations.build(:color => color, :distance => d)
+names.each do |name|
+  course = Course.new(:name => name, :par => 72)
+  pars.each_with_index do |par, i|
+    course.holes.build(:par => par, :position => i+1, :handicap => handicaps[i])
   end
-end
+  distances.each do |color, dists|
+    dists.each_with_index do |d, i|
+      course.holes[i].hole_informations.build(:color => color, :distance => d)
+    end
+  end
 
-aigle.save!
+  course.save!
+end
