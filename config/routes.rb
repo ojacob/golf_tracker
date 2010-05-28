27e41d2348a220c1fa95ec4467bd73f377cf2820
Olivier Jacob <ojacob@octo.com>
2010-05-28 10:27:54 +0200

    Player stats skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :courses, :has_many => :holes
   map.resources :rounds, :has_many => :round_holes
+  
+  # Stats
+  map.stats '/stats', :controller => 'stats', :action => 'index'
 
   # Application home page
   map.root :controller => "courses"
